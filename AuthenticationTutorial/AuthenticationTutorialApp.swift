47a440303d77f71819db04248b99531a074154df
pedrodel26 <pedromodesto26@gmail.com>
2024-07-31 21:13:37 -0300

    Initial Commit

---
+++ AuthenticationTutorial/AuthenticationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationTutorialApp.swift
+//  AuthenticationTutorial
+//
+//  Created by Pedro Delmondes  on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
